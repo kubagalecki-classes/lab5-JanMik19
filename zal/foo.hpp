c3828a2e5e0f5b1a4421e806bf97fa059818200d
JanMik19 <j.mikulevic@gmail.com>
2020-12-01 21:48:47 +0000

    first changes

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -1,12 +1,25 @@
 #pragma once
-
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+using namespace std;
+
+vector< char > foo(list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+  vector< char > obj; // wektor objektow typu character(char)
+
+  for(Human& h : people)
+  {
+    h.birthday(); // ikrementacja poprzez wywolanie metody "birthday()"
+    if (h.isMonster() == false)
+      obj.emplace_back('y');
+    else
+ 	    obj.emplace_back('n');
+  }
+  
+  for(auto rh = obj.crbegin(); rh != obj.crend(); rh++)
+    obj.emplace_front(*rh);
+  
+  return obj; // zwrot wektora objektow typu 'char'
 }
